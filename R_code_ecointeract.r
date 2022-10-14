2db099f584cc737668cc5c92343617236b3ef7db
Allison <114755698+ahsermarini@users.noreply.github.com>
2022-10-14 17:24:36 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -1,3 +1,13 @@
 # This is a code for investigating relationships among ecological variables
 
-library(sp)
+library(sp) # you can also make use of require ()
+
+# We are using meuse:
+# search for: "meuse dataset R sp package"
+# https://cran.r-project.org/web/packages/gstat/vignettes/gstat.pdf
+
+# This function is for the data set
+data(meuse)
+meuse
+
+View(meuse)
