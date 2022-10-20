6960a932bfc57b07afe4a563aac6184900c2e33b
Allison <114755698+ahsermarini@users.noreply.github.com>
2022-10-20 17:25:44 +0200

    Update Learning_ggplot.r

--- Learning_ggplot.r
+++ Learning_ggplot.r
@@ -1,1 +1,4 @@
-
+# looking at point pattern ananlysis how individuals are moving and changing their behavior
+# looking at virus and how it spread in 2019
+#buttefly effect - things in nature are not truly random they seem random but very little things are actually random
+#Many things are realted to space and spacial 
