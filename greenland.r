a1fdf7255dcd8e19ba57ace8fc5eb6dabc65e5ef
Allison <114755698+ahsermarini@users.noreply.github.com>
2022-12-02 17:06:43 +0100

    Create greenland.r

---
+++ greenland.r
@@ -0,0 +1,54 @@
+
+# how to download and utilize data from Copernicus
+library(ncdf4) #read nc files
+library(raster) # typical package
+library(ggplot2) # beautiful plots
+library(RStoolbox) # remote sensing functions
+library(viridis) # legends - color gamut
+library(patchwork) # multiframe ggplot
+
+setwd("C:/Users/serma/OneDrive/Documents/RLab_Ecosystems") #setworking directory
+
+lst_2000 <- raster("lst_2000.tif")
+
+# This is the panel
+# this needs to be put together or will not generate plot
+ggplot() +
+geom_raster(lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
+ggtitle("Landsurface Temperature 2000") +
+scale_fill_viridis(option="inferno", direction=-1, alpha=0.8) #the typical viridis common legend if you use -1 it reversees the color also transparency is alpha
+
+p1 <- ggplot() +
+geom_raster(lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
+ggtitle("Landsurface Temperature 2000") +
+scale_fill_viridis(option="inferno", direction=-1, alpha=0.2)
+
+p2 <- ggplot() +
+geom_raster(lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
+ggtitle("Landsurface Temperature 2000") +
+scale_fill_viridis(option="inferno", direction=-1, alpha=0.8)
+
+p1 + p2
+
+# how to upload all the data
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+# how to use the L apply help list files if you have 100 list.filed function - first we make a working directy list firststep
+# all out files start with lst
+rlist <- list.files(pattern="lst")
+rlist
+
+# list of files has been created now you will need to import with lapply use raster to import we are naming all the picture import
+# lapply(X, FUN)
+import <- lapply(rlist, raster)
+
+#named it tgr then stack all the imported images
+TGr <- stack(import)
